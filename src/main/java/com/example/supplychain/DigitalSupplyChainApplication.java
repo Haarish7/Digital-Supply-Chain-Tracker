e82b25a794b796afc1ee0a32f6b6524ae59da5a1
Ronish Raja <ronishraja1@gmail.com>
2025-05-26 11:34:31 +0530

    [Ronish Raja - Added base code]

---
+++ src/main/java/com/example/supplychain/DigitalSupplyChainApplication.java
@@ -0,0 +1,13 @@
+package com.example.supplychain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSupplyChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSupplyChainApplication.class, args);
+	}
+
+}
