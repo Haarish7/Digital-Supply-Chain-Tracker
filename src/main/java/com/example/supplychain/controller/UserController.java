fd00102c2f42c77e8c16cea9e9215daadaf13d55
Harshit Aggarwal <103198026+2003Harshit@users.noreply.github.com>
2025-05-26 15:20:43 +0530

    [Harshit Aggarwal] - Added user entity and role setup

--- src/main/java/com/example/supplychain/controller/UserController.java
+++ src/main/java/com/example/supplychain/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.example.supplychain.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-	
 	@GetMapping("/welcome")
 	public String hello() {
 		return "Welcome to DSCT";
---
+++ src/main/java/com/example/supplychain/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.supplychain.entity;
+
+import com.example.supplychain.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
