0243393f9d45f4dc701fef5771c12198631c1bf6
barnisha03 <bv6925@srmist.edu.in>
2025-05-26 12:44:18 +0530

    [barnisha] Added welcome methord

--- src/main/java/com/example/supplychain/controller/UserController.java
+++ src/main/java/com/example/supplychain/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.example.supplychain.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
+	
+	@GetMapping("/welcome")
+	public String hello() {
+		return "Welcome to DSCT";
+	}
 }
