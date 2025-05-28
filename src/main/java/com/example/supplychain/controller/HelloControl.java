e5fcbf6641741ab8bae78fc1336dac4e807aca72
Yasir Syed <sssy3654@gmail.com>
2025-05-28 11:46:21 +0530

    Added HelloControl.java to controller folder

---
+++ src/main/java/com/example/supplychain/controller/HelloControl.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@Tag(name = "Hello API", description = "Simple Hello API to test Swagger")
+public class HelloControl {
+
+    @Operation(summary = "Say hello to a user")
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam String name) {
+        return "Hello, " + name + "! Swagger is working.";
+    }
+}
