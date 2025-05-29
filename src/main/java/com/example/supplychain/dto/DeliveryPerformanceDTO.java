b0007d17b48266568aa0e3ba15204b84090df079
vaishnavbusa18 <vaishnavbusa@gmail.com>
2025-05-29 14:20:55 +0530

    [Vaishnav]

--- src/main/java/com/example/supplychain/dto/DeliveryPerformanceDTO.java
+++ src/main/java/com/example/supplychain/dto/DeliveryPerformanceDTO.java
@@ -2,13 +2,48 @@ package com.example.supplychain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class DeliveryPerformanceDTO {
-    private String supplierName;
-    private long totalShipments;
-    private long onTimeDeliveries;
-    private long lateDeliveries;
-    private double averageDelayInHours;
+private String supplierName;
+private long totalShipments;
+private long onTimeDeliveries;
+private long lateDeliveries;
+private double averageDelayInHours;
+public DeliveryPerformanceDTO(String supplierName2, long total, long onTime, long late, double avgDelay) {
+	
+}
+public String getSupplierName() {
+	return supplierName;
+}
+public void setSupplierName(String supplierName) {
+	this.supplierName = supplierName;
+}
+public long getTotalShipments() {
+	return totalShipments;
+}
+public void setTotalShipments(long totalShipments) {
+	this.totalShipments = totalShipments;
+}
+public long getOnTimeDeliveries() {
+	return onTimeDeliveries;
+}
+public void setOnTimeDeliveries(long onTimeDeliveries) {
+	this.onTimeDeliveries = onTimeDeliveries;
+}
+public long getLateDeliveries() {
+	return lateDeliveries;
+}
+public void setLateDeliveries(long lateDeliveries) {
+	this.lateDeliveries = lateDeliveries;
+}
+public double getAverageDelayInHours() {
+	return averageDelayInHours;
+}
+public void setAverageDelayInHours(double averageDelayInHours) {
+	this.averageDelayInHours = averageDelayInHours;
+}
 }
